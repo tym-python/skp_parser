e6d19281e75a42032316a24de663f62367dc06af
Piece One <try@qq.com>
2026-09-11 11:17:05 +0800

    220-database

--- util/db_config.py
+++ util/db_config.py
@@ -3,6 +3,6 @@ mysql = {'local': {'host': '127.0.0.1', 'port': 3306, 'user': 'root', 'password'
          # 'db252': {'host': '192.168.1.252', 'port': 3306, 'user': 'root', 'password': '123asd123asd',
          #           'database': 'suidaobig', 'charset': 'utf8mb4'},
          'db220': {'host': '192.168.1.220', 'port': 3306, 'user': 'root', 'password': '123asdzxc',
-                   'database': 'suitang', 'charset': 'utf8mb4'}
+                   'database': 'suidaobig', 'charset': 'utf8mb4'}
 
 }
